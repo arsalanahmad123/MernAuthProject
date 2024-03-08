90e2dce00c68582fd3255297ac326558be216424
Arsalan <102609746+arsalanahmad123@users.noreply.github.com>
2024-03-08 18:41:07 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 // testing homepage for api 
 
 app.use('/api/v2', authRoute);
-app.get('/', (req,res) => { res.send('Hello World') })
+app.get('/hello', (req,res) => { res.send('Hello World') })
 
 
 
